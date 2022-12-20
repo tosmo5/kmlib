9f1fd4a3480274564bb74dba2614197f694b6bbc
tosmo5 <tosmo5@163.com>
2022-12-20 16:32:04 +0800

    JvmTimeUtils改名为JTimeUtils

--- src/jvmMain/kotlin/com/tosmo/kmlib/time/date/Extension.kt
+++ src/jvmMain/kotlin/com/tosmo/kmlib/time/date/Extension.kt
@@ -1,9 +1,9 @@
 package com.tosmo.kmlib.time.date
 
-import com.tosmo.kmlib.time.JvmTimeUtils
+import com.tosmo.kmlib.time.JTimeUtils
 import com.tosmo.kmlib.time.KTemporalPattern
 import java.util.*
 
 fun KDate.Companion.from(date: Date): KDate {
-    return KTemporalPattern.DEFAULT_DATE_PATTERN.let { KDate(JvmTimeUtils.format(date, it), it) }
+    return KTemporalPattern.DEFAULT_DATE_PATTERN.let { KDate(JTimeUtils.format(date, it), it) }
 }
