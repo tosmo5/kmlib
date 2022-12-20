f181398fce862b092357a463e5b786b10cf3ce11
tosmo5 <tosmo5@163.com>
2022-12-20 16:44:21 +0800

    新增函数运行时间计时方法

--- src/commonMain/kotlin/com/tosmo/kmlib/time/KTimeUtils.kt
+++ src/commonMain/kotlin/com/tosmo/kmlib/time/KTimeUtils.kt
@@ -5,4 +5,22 @@ package com.tosmo.kmlib.time
  *
  * @author Thomas Miao
  */
-object KTimeUtils : InstantInterface by KInstantImpl
+object KTimeUtils : InstantInterface by KInstantImpl {
+
+    /**
+     * 返回[action]运行的毫秒数
+     */
+    fun onMilli(action: () -> Unit): Long {
+        return currentMilli.let {
+            action()
+            currentMilli - it
+        }
+    }
+
+    /**
+     * 返回[action]运行的秒数
+     */
+    fun onSec(action: () -> Unit): Float {
+        return onMilli(action) / 1000f
+    }
+}
